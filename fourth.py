f1f99ef6ff76cf7a50495d05c3a72a25193f0d8d
cernej <cernej@email.cz>
2024-10-23 23:10:59 +0000

    upravy

--- fourth.py
+++ fourth.py
@@ -2,7 +2,7 @@ def je_tah_mozny(figurka, cilova_pozice, obsazene_pozice):
     """
     Ověří, zda se figurka může přesunout na danou pozici.
 
-    :param figurka: Slovník s informacemi o figurce (typ, barva, pozice).
+    :param figurka: Slovník s informacemi o figurce (typ, pozice).
     :param cilova_pozice: Cílová pozice na šachovnici jako n-tice (řádek, sloupec).
     :param obsazene_pozice: Množina obsazených pozic na šachovnici.
     
@@ -13,8 +13,8 @@ def je_tah_mozny(figurka, cilova_pozice, obsazene_pozice):
 
 
 if __name__ == "__main__":
-    pesec = {"typ": "pěšec", "barva": "bílá", "pozice": (2, 2)}
-    jezdec = {"typ": "jezdec", "barva": "černá", "pozice": (3, 3)}
+    pesec = {"typ": "pěšec", "pozice": (2, 2)}
+    jezdec = {"typ": "jezdec", "pozice": (3, 3)}
     obsazene_pozice = {(2, 2), (8, 1), (3, 3), (5, 4)}
 
     print(je_tah_mozny(pesec, (3, 2), obsazene_pozice))  # True
@@ -24,3 +24,4 @@ if __name__ == "__main__":
     print(je_tah_mozny(jezdec, (4, 4), obsazene_pozice))  # False, jezdec se pohybuje ve tvaru pismene L (2 pozice jedním směrem, 1 pozice druhým směrem)
     print(je_tah_mozny(jezdec, (5, 4), obsazene_pozice))  # False, tato pozice je obsazená jinou figurou
     print(je_tah_mozny(jezdec, (1, 2), obsazene_pozice))  # True
+    print(je_tah_mozny(jezdec, (9, 3), obsazene_pozice))  # False, je to pozice mimo šachovnici

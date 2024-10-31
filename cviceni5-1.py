75c2e866bfdd77b8b931f5ba802f510287ae9d73
cernej <cernej@email.cz>
2024-10-31 10:34:38 +0000

    fuj

--- cviceni5-1.py
+++ cviceni5-1.py
@@ -1,15 +1,58 @@
 import sys
+import csv
 
 def nacti_csv(soubor):
-    pass
+    data = []
+    with open(soubor, "r") as fp:
+        reader = csv.reader(fp)
+        for row in reader:
+            data.append(row)
+    return data
 
 
 def spoj_data(*data):
-    pass
+    {
+        "Novak": {
+            "jmeno": "Jan",
+            "prijmeni": "Novak",
+            "vek": "20",
+            "rocnik": 1
+        }
+    }
+
+    vysledek = {}
+
+    keys = set()
+    for d in data:
+        for k in d[0]:
+            keys.add(k)
+        for v in d[1:]:
+
+    
+
+
+    print(keys)
+
+    tmp = {}
+    for d in data:
+        for v in zip(*d):
+            tmp[v[0]] = v[1:]
+    vysledek = []
+    vysledek.append(list(tmp.keys()))
+    for v in zip(*tmp.values()):
+        vysledek.append(v)
+    
+    return vysledek
+
+
+# def spoj_data(*data):
+#     return [['jmeno', 'prijmeni', 'vek', 'rocnik'], ['Tomas', 'Novak', '20', '1'], ['Jan', 'Dvorak', '25', '2'],  ['Alice', 'Novotna', '', '1']]
 
 
 def zapis_csv(soubor, data):
-    pass
+    with open(soubor, "w") as fp:
+        writer = csv.writer(fp)
+        writer.writerows(data)
 
 
 if __name__ == "__main__":
@@ -19,6 +62,9 @@ if __name__ == "__main__":
         csv_data1 = nacti_csv(soubor1)
         csv_data2 = nacti_csv(soubor2)
         vysledna_data = spoj_data(csv_data1, csv_data2)
-        zapis_csv(vysledna_data)
-    except Exception:
-        pass
+        print(vysledna_data)
+        zapis_csv("vysledny_excel.csv", vysledna_data)
+    except IndexError:
+        print("Zadej 2 vstupni soubory typu csv")
+    except FileNotFoundError:
+        print("Soubor neexistuje")

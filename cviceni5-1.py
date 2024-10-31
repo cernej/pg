b3dfe654eafeb576d13eabb156699aacb4967c3f
cernej <cernej@email.cz>
2024-10-31 11:42:55 +0000

    cviceni5

--- cviceni5-1.py
+++ cviceni5-1.py
@@ -3,68 +3,45 @@ import csv
 
 def nacti_csv(soubor):
     data = []
-    with open(soubor, "r") as fp:
-        reader = csv.reader(fp)
-        for row in reader:
-            data.append(row)
+    # nacist data ze souboru
     return data
 
 
-def spoj_data(*data):
-    {
-        "Novak": {
-            "jmeno": "Jan",
-            "prijmeni": "Novak",
-            "vek": "20",
-            "rocnik": 1
-        }
-    }
+def spoj_data(data1, data2):
+    if not data1 or not data2:
+        return data1 if data1 else data2
 
-    vysledek = {}
+    # Získání hlaviček a dat z obou souborů
+    headers1, *rows1 = data1
+    headers2, *rows2 = data2
 
-    keys = set()
-    for d in data:
-        for k in d[0]:
-            keys.add(k)
-        for v in d[1:]:
+    # Vytvoření slovníků pro snadné vyhledávání
+    data_dict1 = {tuple(row[:2]): row[2:] for row in rows1}
+    data_dict2 = {tuple(row[:2]): row[2:] for row in rows2}
 
-    
+    # Spojení hlaviček a inicializace výsledného seznamu
+    all_headers = list(dict.fromkeys(headers1 + headers2))  # Eliminuje duplicity a udržuje pořadí
+    result_data = [all_headers]
 
+    # Spojení dat
+    all_keys = set(data_dict1.keys()).union(set(data_dict2.keys()))
 
-    print(keys)
+    for key in all_keys:
+        new_row = list(key) + [data_dict1.get(key, [''])[0]] + [data_dict2.get(key, [''])[0]]
+        result_data.append(new_row)
 
-    tmp = {}
-    for d in data:
-        for v in zip(*d):
-            tmp[v[0]] = v[1:]
-    vysledek = []
-    vysledek.append(list(tmp.keys()))
-    for v in zip(*tmp.values()):
-        vysledek.append(v)
-    
-    return vysledek
-
-
-# def spoj_data(*data):
-#     return [['jmeno', 'prijmeni', 'vek', 'rocnik'], ['Tomas', 'Novak', '20', '1'], ['Jan', 'Dvorak', '25', '2'],  ['Alice', 'Novotna', '', '1']]
+    return result_data
 
 
 def zapis_csv(soubor, data):
-    with open(soubor, "w") as fp:
-        writer = csv.writer(fp)
-        writer.writerows(data)
+    pass
 
 
 if __name__ == "__main__":
-    try:
-        soubor1 = sys.argv[1]
-        soubor2 = sys.argv[2]
-        csv_data1 = nacti_csv(soubor1)
-        csv_data2 = nacti_csv(soubor2)
-        vysledna_data = spoj_data(csv_data1, csv_data2)
-        print(vysledna_data)
-        zapis_csv("vysledny_excel.csv", vysledna_data)
-    except IndexError:
-        print("Zadej 2 vstupni soubory typu csv")
-    except FileNotFoundError:
-        print("Soubor neexistuje")
+
+    soubor1 = sys.argv[1]
+    soubor2 = sys.argv[2]
+    csv_data1 = nacti_csv(soubor1)
+    csv_data2 = nacti_csv(soubor2)
+    vysledna_data = spoj_data(csv_data1, csv_data2)
+    zapis_csv("vysledny_excel.csv", vysledna_data)

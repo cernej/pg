6c1c6b7935a19e6325d4bc433c3b79bed900c1a9
cernej <cernej@email.cz>
2024-11-14 09:40:38 +0000

    requests example

--- cviceni6-1.py
+++ cviceni6-1.py
@@ -1,4 +1,6 @@
+import sys
 import requests
+from lxml import html
 
 """
 program stahne url a z nej vrati vsechny nadpisy:
@@ -9,10 +11,27 @@ program stahne url a z nej vrati vsechny nadpisy:
 <h5>Nejmensi nadpis</h5>
 """
 def stahni_url_a_vrat_nadpisy(url):
-    pass
+    nadpisy = []
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print(f'nastala chyba, nepodarilo se pripojit na {url}')
+        return []
+    if response.status_code != 200:
+        print(f'nastala chyba, http code: {response.status_code}')
+        return []
+    
+    root = html.fromstring(response.content)
+    for h in ("h1", "h2", "h3", "h4", "h5"):
+        for h1 in root.xpath(f"//{h}"):
+            h1_text = h1.text_content().strip()
+            if h1_text:
+                nadpisy.append(h1_text)
+
+    return nadpisy
 
 
 if __name__ == "__main__":
-    url = input("Zadej url: ")
+    url = sys.argv[1]
     nadpisy = stahni_url_a_vrat_nadpisy(url)
     print(nadpisy)

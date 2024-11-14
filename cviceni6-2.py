a41c59cef622dbe1b28ac3d4dd2f5dd70e19a3e5
cernej <cernej@email.cz>
2024-11-14 12:18:32 +0000

    cviceni

--- cviceni6-2.py
+++ cviceni6-2.py
@@ -7,23 +7,8 @@ url = "https://db.carnewschina.com/suggest?q="
 
 def download_json_and_parse_brands(prefix):
     # stahneme url + prefix
-    url_prefix = url + prefix
-    response = requests.get(url_prefix)
-    # kontrola navratove hodnoty
-    if response.status_code != 200:
-        print("chyba")
-        return []
-    # stahneme json
-    data = response.json()
-    
+
     result = []
-
-    # vytahneme nazvy brandu ["brans"]["name"]
-    brands = data["brands"]
-    for brand in brands:
-        name = brand["name"]
-        result.append(name)
-
     return result
 
 

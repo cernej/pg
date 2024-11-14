9e847a20fbcacd87ce7b2e9bd9ff84cae838ed72
cernej <cernej@email.cz>
2024-11-14 13:05:23 +0000

    cviceni 6 json

--- cviceni6-3.py
+++ cviceni6-3.py
@@ -1,11 +1,24 @@
 import json
 
 data = [
-    {'Jmeno': 'Alice', 'vek': 20}, 
-    {'Jmeno': 'Bob', 'vek': 25}, 
-    {'Jmeno': 'Dave', 'vek': 31}, 
-    {'Jmeno': 'Monika', 'vek': 28}
+    {'Jmeno': 'Alice', 'vek': 20, 'student': True}, 
+    {'Jmeno': 'Bob', 'vek': 25, 'student': False}, 
+    {'Jmeno': 'Dave', 'vek': 31, 'student': False}, 
+    {'Jmeno': 'Monika', 'vek': 28, 'student': True}
 ]
 
 if __name__ == "__main__":
-    pass
+    
+    data.pop(3)
+    print(data)
+    
+    with open("data.json", "w") as fp:
+        fp.write(json_data)
+
+    with open("data.json", "r") as fp:
+        json_data2 = fp.read()
+        data2 = json.loads(json_data2)
+    
+    data2.pop(0)
+    print(data2)
+

f652108c9a1bcb6e868e488a40483abb5ab1629f
cernej <cernej@email.cz>
2024-11-14 10:15:57 +0000

    cviceni 6

--- cviceni6-2.py
+++ cviceni6-2.py
@@ -7,17 +7,24 @@ url = "https://db.carnewschina.com/suggest?q="
 
 def download_json_and_parse_brands(prefix):
     # stahneme url + prefix
-    response = requests.get( ... )
+    url_prefix = url + prefix
+    response = requests.get(url_prefix)
+    # kontrola navratove hodnoty
+    if response.status_code != 200:
+        print("chyba")
+        return []
     # stahneme json
-    json_data = response.json()
-    # zkonvertujeme na data
-    json.loads()
-
-    brands = []
+    data = response.json()
+    
+    result = []
 
     # vytahneme nazvy brandu ["brans"]["name"]
+    brands = data["brands"]
+    for brand in brands:
+        name = brand["name"]
+        result.append(name)
 
-    return brands
+    return result
 
 
 if __name__ == "__main__":
@@ -27,3 +34,4 @@ if __name__ == "__main__":
     for brand in brands:
         print(brand)
 
+    # pro prefix "ni" mi to vypise Nissan a Nio

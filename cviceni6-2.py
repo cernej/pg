05d91df5411bd42624ec6c1f183e88580fbe2260
cernej <cernej@email.cz>
2024-11-21 08:52:34 +0000

    upravy

--- cviceni6-2.py
+++ cviceni6-2.py
@@ -5,10 +5,22 @@ import requests
 url = "https://db.carnewschina.com/suggest?q="
 
 
-def download_json_and_parse_brands(prefix):
+def download_json_and_parse_brands(abrakadabra):
     # stahneme url + prefix
 
+    response = requests.get(url + abrakadabra)
+    if response.status_code != 200:
+        print('chyba')
+        return []
+    
+    data = json.loads(response.content)
+
     result = []
+
+    brands = data['brands']
+    for brand in brands:
+        result.append(brand['name'])
+    
     return result
 
 

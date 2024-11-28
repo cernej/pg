dd62e47c1bbc15fb14877cb0c551c64af7df572c
cernej <cernej@email.cz>
2024-11-28 07:33:14 +0000

    testovani

---
+++ test.py
@@ -0,0 +1,16 @@
+from aplikace import soucet, fibonachi
+
+def test_soucet_2():
+    assert soucet(1, 1) == 2
+
+def test_soucet_3():
+    assert soucet(1, 2) == 3
+
+def test_fibonachi_5():
+    assert fibonachi(5) == [1, 1, 2, 3, 5]
+
+def test_fibonachi_10():
+    assert fibonachi(10) == [1, 1, 2, 3, 5, 8]
+
+
+

c6c7e0eef04dfc6cb5e3cc2eb8d7436fd0566ff3
cernej <cernej@email.cz>
2024-11-28 12:14:55 +0000

    cviceni 8

--- cviceni8.py
+++ cviceni8.py
@@ -1,43 +1,27 @@
 def obvod_ctverce(delka_strany):
     # funkce vypocita obvod ctverce z delky jeho strany
-    return 4 * delka_strany
+    return 0
 
 
 def obsah_ctverce(delka_strany):
     # funkce vypocita obsah ctverce z delky jeho strany
-    return delka_strany ** 2
+    return 0
 
 
 def pocet_pismen(text, pismeno):
     # funkce vrati pocet vyskytu pismene v textu
-    pocet = 0
-    for p in text:
-        if p == pismeno:
-            pocet += 1
-    return pocet
+    return 0
 
 
 def index_pismene(text, pismeno):
     # funkce vrati indexy daneho pismene v textu, tzn. pro text="ahoj, honzo" a pismeno="h" vrati [1, 6]
-    indexy = []
-    i = 0
-    while i < len(text):
-        if text[i] == pismeno:
-            indexy.append(i)
-        i += 1
-    return indexy
+    return []
 
 
 def fibonachi(maximum):
     # funkce vrati fibonachiho posloupnost, pro maximum=5 -> [1, 1, 2, 3, 5]
-    fib = [1, 1]
-    soucet = fib[-2] + fib[-1]
-    while soucet <= maximum:
-        fib.append(soucet)
-        soucet = fib[-2] + fib[-1]
-    return fib
+    return []
 
 
 if __name__ == "__main__":
-    #index_pismene("ahoj, honzo", "h")
-    fibonachi(5)
+    index_pismene("ahoj, honzo", "h")
